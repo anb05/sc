5c7aa7f849437ad177e7a7a3c2c8a50896366df6
anb05 <alexandr05@list.ru>
2018-04-20 16:44:55 +0300

    Changed WavRaw files

--- WavRaw.cpp
+++ WavRaw.cpp
@@ -8,26 +8,58 @@ using namespace std;
 using std::ifstream;
 using std::ios;
 
-//template <typename T>
-//WavRaw<T>::WavRaw()
+// Конструкор умолчаний
 WavRaw::WavRaw()
 {
-    //itsContainer = new T;
     itsContainer = new short[1];
     itsContainer[0] = 0;
     containerLength = 1;
 }
 
-//template <typename T>
-//WavRaw<T>::WavRaw(string sourceName, unsigned int addres, unsigned int length)
+// Конструктор создающий контейнер из WAV файла
 WavRaw::WavRaw(string sourceName, unsigned int addres, unsigned int length)
 {
     cout << " конструктор WavRaw" << endl;
-    extractData(sourceName, addres, length);
+    containerLength = length / sizeof(short);
+    itsContainer = new short[containerLength];
+
+    extractData(sourceName, addres, containerLength);
+}
+
+// Конструктор создающий контейнер из массива с отсчётами
+// В качестве аргумента передается указатель на массив отсчётов амплитуд
+WavRaw::WavRaw(const short* const ptrShortArray, const unsigned int & numOfElements)
+{
+    containerLength = numOfElements;
+    itsContainer = new short[containerLength];
+    for (unsigned int counter = 0; counter < containerLength; ++counter) {
+        itsContainer[counter] = ptrShortArray[counter];
+    }
+}
+
+// Конструктор копирования
+WavRaw::WavRaw(const WavRaw & copyWavRaw)
+{
+    containerLength = copyWavRaw.containerLength;
+    itsContainer = new short[containerLength];
+    for (unsigned int counter = 0; counter < containerLength; ++counter) {
+        itsContainer[counter] = copyWavRaw[counter];
+    }
+}
+
+// Закрытый конструктор, создающий контейнер заданой длины
+// с элементами, рвными 0
+WavRaw::WavRaw(unsigned int requiredLength) :
+    containerLength(requiredLength)
+{
+    itsContainer = new short[containerLength];
+    for (unsigned int counter = 0; counter < containerLength; ++counter) {
+        itsContainer[counter] = 0;
+    }
 }
 
 
-//template <typename T>
+// Деструкор
 WavRaw::~WavRaw()
 {
     delete [] itsContainer;
@@ -35,43 +67,113 @@ WavRaw::~WavRaw()
     cout << "destructor WavRaw" << endl;
 }
 
-//template <typename T>
-//void WavRaw<T>::extractData(string sourceName, unsigned int addres, unsigned int length)
+/* Перегруженные операторы */
+
+// Неконстантный оператор индексирования,
+// возвращает ссылку на отсчёт, который можно изменить
+short & WavRaw::operator[](int offset)
+{
+    if (offset < containerLength) {
+        return itsContainer[offset];
+    }
+    return itsContainer[containerLength - 1];
+}
+
+// Константный оператор индексирования,
+// используется для константных объктов (см. конструктор-копировщик)
+short WavRaw::operator[](int offset) const
+{
+    if (offset < containerLength) {
+        return itsContainer[offset];
+    }
+    return itsContainer[containerLength - 1];
+}
+
+// Оператор создаёт новый объект WavRaw в контейнере которого содержится
+// объединенные отсчеты двух объектов WavRaw
+WavRaw WavRaw::operator+ (const WavRaw & rhs)
+{
+    unsigned int totalLength = containerLength + rhs.containerLength;
+    WavRaw tempContainer(totalLength);
+    unsigned int counter = 0;
+    unsigned int rhsCounter = 0;
+    for (counter = 0; counter < containerLength; ++ counter) {
+        tempContainer.itsContainer[counter] = itsContainer[counter];
+    }
+    for (rhsCounter = 0; rhsCounter < rhs.containerLength; ++rhsCounter, ++counter) {
+        tempContainer.itsContainer[counter] = rhs[rhsCounter];
+    }
+    return tempContainer;
+}
+
+// К текущему контейнеру добаляет отсчеты из контейнера аргумента
+void WavRaw::operator+= (const WavRaw & rhs)
+{
+    unsigned int totalLength = containerLength + rhs.containerLength;
+    WavRaw tempContainer(totalLength);
+    unsigned int counter = 0;
+    unsigned int rhsCounter = 0;
+    for (counter = 0; counter < containerLength; ++counter) {
+        tempContainer[counter] = itsContainer[counter];
+    }
+    for (rhsCounter = 0; rhsCounter < rhs.containerLength; ++rhsCounter, ++counter) {
+        tempContainer[counter] = rhs[rhsCounter];
+    }
+}
+
+        // 
+WavRaw & WavRaw::operator= (const WavRaw & rhs)
+{
+    if (this == &rhs) {
+        return *this;
+    }
+
+    containerLength = rhs.containerLength;
+    delete[] itsContainer;
+    itsContainer = new short[containerLength];
+    for (unsigned int counter = 0; counter < containerLength; ++ counter) {
+        itsContainer[counter] = rhs[counter];
+    }
+    return *this;
+}
+
 void WavRaw::extractData(string sourceName, unsigned int addres, unsigned int length)
 {
     cout << "WavRaw::sourceName = " << sourceName << endl;
     cout << "WavRaw::addres = " << addres << endl;
+    unsigned int delta = 0;
+
+    if (length > containerLength) {
+        length = containerLength;
+    }
+    else {
+        delta = containerLength - length;
+    }
     cout << "WavRaw::length = " << length << endl;
-    cout << "WavRaw::containerLength = " << containerLength << endl;
 
-    //if (containerLength > 0) {
-    //    delete[] itsContainer;
-    //}
-
-    containerLength = length / sizeof(short);
-    cout << "WavRaw::containerLength = " << containerLength << endl;
-
-    itsContainer = new short[containerLength];
-
-
-
+    unsigned int counter = 0;
     ifstream inpFile;
     inpFile.open(sourceName, ios::in | ios::binary);
-    /*
     if (inpFile.is_open()) {
         inpFile.seekg(addres);
-        //for (unsigned int counter = 0; counter < containerLength; ++ counter) {
-        //    inpFile.read((char*)&itsContainer[counter], sizeof(short));
-        //}
+        for (counter = 0; counter < length; ++ counter) {
+            inpFile.read((char*)&itsContainer[counter], sizeof(short));
+        }
     }
-    */
     inpFile.close();
 
+    if (delta > 0) {
+        for (; counter < containerLength; ++ counter) {
+            itsContainer[counter] = 0;
+        }
+    }
 
 
-    //for (int i = containerLength - 10 ; i < containerLength; ++i) {
-        //cout << "itsContainer[" << i << "] = " << hex << itsContainer[i] << endl;
-    //}
+
+
+    for (int i = containerLength - 10 ; i < containerLength; ++i) {
+        cout << "itsContainer[" << i << "] = " << hex << itsContainer[i] << endl;
+    }
 }
 
 //template <typename T>
--- WavRaw.hpp
+++ WavRaw.hpp
@@ -16,12 +16,14 @@ using std::string;
 class WavRaw : public SourceData
 {
     public:
-        // Конструкторы деструктор
+        /* Конструкторы деструктор */
+
         // Конструкторы умолчаний, создает контейнер длиной в 1 ячейку
         // со значением 0.
         // Значение containerLength устанавливаетмя равным 1
         WavRaw();
 
+        // Конструктор создающий контейнер с WAV отсчетами из WAV файла
         // В конструтр передаются:
         //    - нименование файла, из которого осуществляется чтение;
         //    - начальное смещение в байтах от начала файла (для wav файла 44);
@@ -32,7 +34,38 @@ class WavRaw : public SourceData
         // Следовательно общее количество отсчётов равно length/2 или 
         // length / sizeof(short)
         WavRaw(string sourceName, unsigned int addres, unsigned int length);
+
+        // Конструктор создающий контейнер из массива с отсчётами
+        // В качестве аргументов передаются:
+        //   - указатель на массив отсчётов амплитуд;
+        //   - количество элементов в массиве
+        WavRaw(const short* const ptrShortArray, const unsigned int & numOfElements);
+
+        // Конструктор копирования
+        WavRaw(const WavRaw & copyWavRaw);
+
+        // Деструктор
         ~WavRaw();
+
+        /* Перегруженные операторы */
+
+        // Неконстантный оператор индексирования,
+        // возвращает ссылку на отсчёт, который можно изменить
+        short & operator[](int offset);
+
+        // Константный оператор индексирования,
+        // используется для константных объктов (см. конструктор-копировщик)
+        short operator[](int offset) const;
+
+        // Оператор создаёт новый объект WavRaw в контейнере которого содержится
+        // объединенные отсчеты двух объектов WavRaw
+        WavRaw operator+ (const WavRaw &);
+
+        // К текущему контейнеру добаляет отсчеты из контейнера аргумента
+        void operator+= (const WavRaw &);
+
+        // Оператор копирования
+        WavRaw & operator= (const WavRaw &);
 
         // Переопределяемые методы
 
@@ -40,8 +73,15 @@ class WavRaw : public SourceData
         // Исходными данными являются:
         //    - нименование файла, из которого осуществляется чтение;
         //    - начальное смещение в байтах от начала файла (для wav файла 44);
-        //    - длина массива информации в байтах.
-        void extractData(string sourceName, unsigned int addres, unsigned int length);
+        //    - количество извлекаемых отсчётов.
+        //
+        // Если значение length (количество извлекаемых отсчетов)
+        // больше размера контейнера (containerLength),
+        // то будет извлечено только containerLength отсчетов.
+        // Если значение length меньше containerLength,
+        // то оставшиеся значения будут замещены нулями
+        //
+        void extractData(string sourceName, unsigned int addres = 0, unsigned int length = 0);
 
 
         //T* readAllData(void) const;
@@ -56,6 +96,10 @@ class WavRaw : public SourceData
 
         // Количество 16-битных отсчётов в контейнере
         unsigned int containerLength;
+
+        // Закрытый конструктор, создающий контейнер заданой длины
+        // с элементами, рвными 0
+        WavRaw(unsigned int requiredLength);
 };
 
 #endif
